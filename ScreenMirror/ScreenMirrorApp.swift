e2f890c5307114b6035de873faacab24627e1594
Kiri4of <kopak7890@gmail.com>
2025-05-23 15:23:25 +0100

    Initial Commit

---
+++ ScreenMirror/ScreenMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenMirrorApp.swift
+//  ScreenMirror
+//
+//  Created by Kiri4of on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
